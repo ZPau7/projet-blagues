fd6b1ed1007a52644195d43379960398c8f6bf2b
Alexiane <alexiane.auger-berteaux@esme.fr>
2025-03-12 11:28:44 +0100

    add_conflit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyjokes
 
 def tell_joke():
     print(pyjokes.get_joke(language=lang))
-
+    print("Hello")
 if __name__ =="__main__":
     lang = input("Choisissez une langue (en, de, es, it, gl): ")
     tell_joke(lang)
